0bda1636c489620a079c451576302f1305762834
Integral Game Console and Iinnexxyy Video Arcade Game Machine <integralgameconsoleIoT@gmail.com>
2021-05-15 17:07:20 +0900

    C Files (Libraries)
    C Files (Libraries)
    Embedded C integer, float code for SoC Test

---
+++ C_files/int64/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  long long num1 = 2000000000;
+  long long num2 = 2000000000;
+  long long num3 = 2000000000;
+  long long sum;
+
+  sum = num1 + num2 + num3;
+  printf("%d\n", sum);
+  return 0;
+
+}
